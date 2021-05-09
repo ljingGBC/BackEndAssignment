293bfcfa0f17273ca9fc161ba53e85a1558e31ab
ljingGBC <lei.jing@georgebrown.ca>
2021-05-08 23:32:07 -0400

    init nodejs project; setup MongoDB Connection".env" and Express web server"server.js"

---
+++ server.js
@@ -0,0 +1,45 @@
+//Define Express Server
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('*', cors());
+
+//Store sensetive information to env variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+//mongoDB Atlas Connection String
+const url = process.env.MONGODB_URL;
+
+//Connect to mongoDB Atlas
+const connect = mongoose.connect(url, 
+{ 
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+});
+
+connect.then((db) => {
+      console.log('Connected correctly to MongoDB!');
+}, (err) => {
+      console.log(err);
+});
+
+// simple route to test connection
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to Lei Jing's application." });
+});
+
+const port = process.env.PORT;
+app.listen({ port }, () =>
+  //http://localhost:5000
+  console.log(`Server is running on port ${port}`));
