3be4b295a964a3e5f8c67d72c279983865a984fb
ljingGBC <lei.jing@georgebrown.ca>
2021-05-09 06:18:27 -0400

    Initialize Mongoose

---
+++ models/index.js
@@ -0,0 +1,11 @@
+// Initialize Mongoose
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+
+db.mongoose = mongoose;
+
+db.user = require("./user");
+
+module.exports = db;
